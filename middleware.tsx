cf2266bead6359427c8f2f99c768dda3a492efef
Jouhar <majouharanchalan1998@gmail.com>
2025-02-05 21:25:12 +0530

    middleware added

---
+++ middleware.tsx
@@ -0,0 +1,33 @@
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+import { withAuth } from "next-auth/middleware";
+
+const PUBLIC_FILE = /\.(.*)$/;
+export default async function middleware(
+  req: NextRequest,
+  event: NextFetchEvent
+) {
+  // const pathname = req.nextUrl.pathname;
+  // const token = await getToken({ req });
+
+  // const isAuthenticated = !!token;
+
+
+
+  // if (
+  //   pathname == "/" ||
+  //   pathname.startsWith("/_next") ||
+  //   pathname.includes("/api") ||
+  //   PUBLIC_FILE.test(pathname) ||
+  //   isAuthenticated
+  // ) {
+  //   return NextResponse.next();
+  // }
+  // const authMiddleware = withAuth({
+  //   pages: {
+  //     signIn: "/login",
+  //   },
+  // });
+  // @ts-expect-error
+  return authMiddleware(req, event);
+}

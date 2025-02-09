48a10f6487525c2ca5e2ea5cb982d7b7296e46d4
Jouhar <majouharanchalan1998@gmail.com>
2025-02-09 18:04:01 +0530

    package.json updated to include schema files

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -3,11 +3,8 @@ import { mergeTypeDefs, mergeResolvers } from "@graphql-tools/merge";
 import { loadFilesSync } from "@graphql-tools/load-files";
 import { blogResolvers } from "./resolvers/blog.resolvers";
 import { userResolvers } from "./resolvers/user.resolvers";
-import path from "path";
 
-const typeDefs = mergeTypeDefs(
-  loadFilesSync(path.join(process.cwd(), "./graphql/**/*.graphql"))
-);
+const typeDefs = mergeTypeDefs(loadFilesSync("./graphql/**/*.graphql"));
 const resolvers = mergeResolvers([blogResolvers, userResolvers]);
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 export default schema;
